cf4e27a838375c7123883d5304220dd27b1fb4f0
Twisted07 <iamtwisted07@gmail.com>
2024-08-14 07:51:30 +0100

    update: responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -37,35 +37,34 @@ function App() {
 
   function handleScroll(e) {
     smoothScroll('#contact-me', 800);
-    console.log(e);
   }
 
   return (
     <>
-      <section className="bg-gray-700 text-center flex flex-col justify-center items-center py-56 border-b-4 border-red-300">
-        <h1 className="text-stone-200">Hello, I am IDRIS ABDULLATEEF, a <span className="text-6xl font-bold text-red-300 block">Software Developer</span></h1>
-        <i className="mt-5 mb-10 text-stone-300 w-[40dvw]">who Engineers Tomorrow's solutions, <span className="text-red-300 font-semibold">TODAY.</span></i>
+      <section className="flex flex-col items-center justify-center py-56 text-center bg-gray-700 border-b-2 border-red-300 md:border-b-4">
+        <h1 className="text-xs text-stone-200 md:text-md lg:text-lg">Hello, I am IDRIS ABDULLATEEF, a <span className="block text-2xl font-semibold text-red-300 md:text-4xl lg:text-6xl lg:font-bold">Software Developer</span></h1>
+        <i className="mt-3 mb-8 lg:mt-5 lg:mb-10 text-stone-300 w-[50dvw] lg:w-[40dvw] md:text-md lg:text-lg text-xs">who Engineers Tomorrow's solutions, <span className="font-semibold text-red-300">TODAY.</span></i>
 
-        <a href={resume} download={"Abdullateef_Idris_Resume"} className="pop rounded-full border border-red-300 py-2 text-red-300 px-5">Download My Resume</a>
+        <a href={resume} download={"Abdullateef_Idris_Resume"} className="px-5 py-2 text-xs text-red-300 border border-red-300 rounded-full lg:text-lg pop">Download My Resume</a>
       </section>
 
-      <section className="px-48 py-24 grid gap-20 grid-cols-3 grid-flow-row-dense bg-stone-200">
-        <div alt="image" className="col-span-1 w-[20rem] h-[20rem] rounded-full border-red-300 border-8" style={{backgroundImage: `url(${photo})`, backgroundPosition: "center", backgroundSize: "cover"}}></div>
-        <div className="col-span-2">
-          <h1 className="text-3xl text-red-400 font-bold mb-10">About Me</h1>
-          <p className="mb-10">
+      <section className="flex flex-col items-center justify-center px-8 py-16 gap-14 md:px-16 lg:gap-56 lg:px-24 lg:py-24 lg:grid lg:grid-cols-3 lg:grid-flow-row bg-stone-200">
+        <div alt="image" className="col-span-1 w-48 h-48 border-4 lg:w-[20rem] lg:h-[20rem] rounded-full border-red-300 lg:border-8" style={{backgroundImage: `url(${photo})`, backgroundPosition: "center", backgroundSize: "cover"}}></div>
+        <div className="col-span-2 text-center lg:text-left">
+          <h1 className="mb-5 text-2xl font-bold text-red-400 lg:text-3xl lg:mb-10">About Me</h1>
+          <p className="mb-10 text-sm">
             I am a skilled Front-End Developer with over 2 years of experience crafting intuitive and visually appealing web applications. I create responsive designs, ensuring seamless user experiences across devices. I am also adept at debugging and optimizing code for performance. I have a strong collaboration skill with a knack for translating client needs into functional, attractive interfaces.
           </p>
           <div>
-          <a href={resume} download={"Abdullateef_Idris_Resume"} className="font-semibold rounded-full border-2 bg-red-400 border-red-400 py-2 text-stone-100 px-5 mr-3">Download My Resume</a>
-          <button onClick={handleScroll} className="pop font-semibold border-2 rounded-full border-red-400 px-5 py-2 text-red-500">Contact Me</button>
+          <a href={resume} download={"Abdullateef_Idris_Resume"} className="px-5 py-2 text-sm font-semibold bg-red-400 border-2 border-red-400 rounded-full lg:mr-3 text-stone-100">Download My Resume</a>
+          <button onClick={handleScroll} className="hidden px-5 py-2 text-sm font-semibold text-red-500 border-2 border-red-400 rounded-full lg:inline-block pop">Contact Me</button>
           </div>
         </div>
       </section>
 
-      <section className="py-24 text-center bg-gray-700 border-y-4 border-red-300">
-        <h1 className="font-bold text-stone-300 text-3xl mb-16">Projects</h1>
-        <div className="flex justify-center items-center gap-10 wrap mx-24">
+      <section className="py-16 text-center bg-gray-700 border-red-300 border-y-2 lg:py-24 lg:border-y-4">
+        <h1 className="mb-8 text-2xl font-bold lg:mb-16 text-stone-300">Projects</h1>
+        <div className="flex flex-col items-center justify-center gap-5 lg:gap-10 lg:flex-row lg:mx-24 wrap">
           <Card
             title={"vacaytion"}
             icon={<FiMapPin size={30} color="rgb(252 165 165 / var(--tw-border-opacity))" />}
@@ -87,29 +86,29 @@ function App() {
         </div>
       </section>
 
-      <section className="pt-24 pb-48 text-center bg-stone-200">
-        <h1 className="font-bold text-red-400 text-3xl mb-20">Contact Me</h1>
-        <div className="flex justify-center items-center gap-20">
+      <section className="pt-16 text-center pb-36 lg:pt-24 lg:pb-48 bg-stone-200">
+        <h1 className="mb-16 text-2xl font-bold text-red-400 md:mb-20 lg:text-3xl">Contact Me</h1>
+        <div className="flex items-center justify-center gap-5 md:gap-20">
           <a href="https://www.linkedin.com/in/abdullateef-idris-aa436b245" className="pop" aria-label="linkedin" target="_blank" rel="noopener noreferrer">
-            <BsLinkedin className="text-red-500" size={50} />
+            <BsLinkedin className="text-red-500 size-8 lg:size-[3rem]" />
           </a>
           <a href="https://wa.me/8118536771" className="pop" target="_blank">
-            <BsWhatsapp className="text-red-500" size={50} />
+            <BsWhatsapp className="text-red-500 size-8 lg:size-[3rem]" />
           </a>
           <a href="https://www.github.com/twisted07" className="pop" id="contact-me">
-            <BsGithub className="text-red-500" size={50} />
+            <BsGithub className="text-red-500 size-8 lg:size-[3rem]" />
           </a>
           <a href="tel:+2348118536771" className="pop" target="_blank">
-            <BsPhone className="text-red-500" size={50} />
+            <BsPhone className="text-red-500 size-8 lg:size-[3rem]" />
           </a>
           <a href="mailto:iamtwisted07@gmail.com" className="pop" target="_blank">
-            <BiLogoGmail className="text-red-500" size={50} />
+            <BiLogoGmail className="text-red-500 size-8 lg:size-[3rem]" />
           </a>
         </div>
       </section>
 
-      <footer className="py-10 text-center border-t-4 border-red-300 bg-gray-700 text-stone-300 flex justify-center">
-        <img src={logo} alt="twisted logo" width={50} height={50} />
+      <footer className="flex justify-center py-10 text-center bg-gray-700 border-t-2 border-red-300 lg:border-t-4 text-stone-300">
+        <img src={logo} alt="twisted logo" className="size-8 lg:size-[3rem]" />
       </footer>
     </>
   )
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Card = ({icon, title, summary, link}) => {
   return (
-    <a href={link} target="_blank" rel="noopener noreferrer" className='pop border-2 border-red-300 justify-center items-start text-stone-200 rounded-md text-left flex flex-col gap-5 px-5 w-[16rem] h-[18rem]'>
+    <a href={link} target="_blank" rel="noopener noreferrer" className='pop border-2 border-red-300 justify-center items-start text-stone-200 rounded-md text-left flex flex-col gap-5 px-5 w-[16rem] h-[18rem] flex-shrink-0'>
         <div className='w-8 h-8 overflow-hidden'>{icon}</div>
-        <h1 className='uppercase font-semibold'>{title}</h1>
+        <h1 className='font-semibold uppercase'>{title}</h1>
         <p className='h-24 text-xs'>{summary}</p>
     </a>
   )
